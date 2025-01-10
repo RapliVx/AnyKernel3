f4bfd9e039614fc9d6d90b9ed3ebf746212190e7
Rapli <154667713+RapliVx@users.noreply.github.com>
2025-01-11 03:32:00 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=HyperX Kernel by ErOneSoul
+kernel.string=Katyusha Kernel by ErOneSoul
 do.devicecheck=1
 do.modules=0
 do.systemless=1
