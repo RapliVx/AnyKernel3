0cffcbd9b6aeed5a2daff07912608d145a180527
Prayoga Imandharu <45396777+Prayogadharu46@users.noreply.github.com>
2025-01-20 02:06:06 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -10,10 +10,10 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=taoyao
+device.name2=Xiaomi12Lite
+device.name3=Mi12L
+device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=
@@ -40,21 +40,24 @@ PATCH_VBMETA_FLAG=auto;
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+# Import Remover
+. /tmp/anykernel/tools/remover.sh;
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+# Backup Boot
+backup_boot() {
+    ui_print "[+] Backup performed on internal storage"
+    sleep 2
+    backup_dir="/sdcard/backup_boot.img"
+    rm -rf $backup_dir
+    dd if=/dev/block/bootdevice/by-name/boot of=$backup_dir
+    if [ -f $backup_dir ] ; then
+    ui_print "[+] Done!"
+    sleep 1
+    else
+    ui_print "[!] Backup failed, Skipping . . . !"
+    sleep 1
+    fi
+}
 
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
