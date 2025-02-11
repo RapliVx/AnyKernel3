40e54b418be7733b0a1f1541a471f9252a1f9272
Rapli <154667713+RapliVx@users.noreply.github.com>
2025-02-11 20:52:39 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=POCO X3 Kernel by xradens
+kernel.string=Trailblazer Kernel by ErOneSoul
 do.devicecheck=1
 do.modules=0
 do.systemless=1
