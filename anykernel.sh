abf7e7a89367b1451feb0ff67cc836a0f18693f0
Rapli <154667713+RapliVx@users.noreply.github.com>
2026-01-10 00:18:58 +0700

    Update kernel string in AnyKernel setup

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Katyusha Kernel by ErOneSoul
+kernel.string=CrimsonGremory by ErOneSoul
 do.devicecheck=1
 do.modules=0
 do.systemless=1
