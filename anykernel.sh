67001c9f6d553e2f28547890195ee820184cf5cd
Rapli <154667713+RapliVx@users.noreply.github.com>
2026-02-04 23:32:48 +0700

    Add files via upload

--- anykernel.sh
+++ anykernel.sh
@@ -1,36 +1,45 @@
-# AnyKernel3 Ramdisk Mod Script
-# osm0sis @ xda-developers
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
 
-## AnyKernel setup
-# begin properties
+### AnyKernel setup
+# global properties
 properties() { '
-kernel.string=Installing Katyusha Kernel System. . .
+kernel.string=
 do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=lavender
-supported.versions=
+device.name1=topaz
+device.name2=tapas
+device.name3=sapphiren
+device.name4=sapphire
+device.name5=xun
+device.name6=creek
+supported.versions=13-16
 supported.patchlevels=
+supported.vendorpatchlevels=
 '; } # end properties
 
-# shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
-ramdisk_compression=auto;
 
-## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference
-. tools/ak3-core.sh;
+### AnyKernel install
+## boot shell variables
+block=boot
+is_slot_device=auto
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
+no_magisk_check=1
 
-## AnyKernel install
-dump_boot;
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh
 
-# migrate from /overlay to /overlay.d to enable SAR Magisk
-if [ -d $ramdisk/overlay ]; then
-  rm -rf $ramdisk/overlay;
-fi;
-
-write_boot;
-## end install
+# boot install
+# Script langsung memproses instalasi tanpa pemilihan kernel
+if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
+  split_boot # for devices with init_boot ramdisk
+  flash_boot # for devices with init_boot ramdisk
+else
+  dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+  write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+fi
+## end boot install
